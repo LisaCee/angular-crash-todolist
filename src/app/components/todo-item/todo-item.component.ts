5de2f87e56345583ac581809a0c8d35ccd7e5398
LisaCee <lisaceedesign@gmail.com>
2019-07-23 20:02:30 -0700

    addtodo

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 
 import { Todo } from 'src/app/models/Todo';
@@ -10,6 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter;
 
   constructor(private todoService: TodoService) { }
 
@@ -33,6 +34,7 @@ export class TodoItemComponent implements OnInit {
   }
 
   onDelete(todo) {
-    console.log('delete')
+    // emiting function to parent todo.component
+    this.deleteTodo.emit(todo);
   }
 }
